57b8207482db2ed8fa7b8e14483bb224c76c555e
Pritesh Patil <pritpatil41@gmail.com>
2025-10-19 23:20:56 +0530

    Add files via upload

---
+++ chatBot.py
@@ -0,0 +1,63 @@
+from http.client import responses
+from idlelib.query import Query
+
+import streamlit
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import streamlit as st
+
+
+load_dotenv()
+
+st.title("💬 I AM PRIT. Throw me questions!")
+st.set_page_config(
+    page_title="Pritesh's Chatbot",
+    page_icon="💬 ",
+    initial_sidebar_state="collapsed",
+    layout="wide"
+)
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+userMessage = st.chat_input("Enter you message")
+
+llm = ChatGroq(
+        model="llama-3.1-8b-instant",
+        temperature=0.0
+    )
+Template = (
+    "Your name is Prit a 'CHATBOT' of guy named Pritesh. Introduce yourself first and then be an arrogant chatbot"
+    "You're 28 years old and have an emmense knowledge in Computer Science. You have done BE in CSE"
+    "You're working in a Software Company and have no competitions"
+    "ANSWERING RULES:"
+    "Do not entertain any more personal questions other than the Given ones"
+    "Only Speak in 1 sentences"
+    "ONLY SPEAK IN HINDI")
+
+if userMessage:
+    st.chat_message("user").markdown(userMessage)
+    st.session_state.chat_history.append({"role":"user","content":userMessage})
+
+    response = llm.invoke(
+        input = [{"role":"system","content":Template},*st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role":"assistant","content":assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
+# Flow:
+# Write Query
+# Display Query
+# Insert Query in ChatHistory
+# Give ChatHistory to LLM
+# Get response
+# Save response in ChatHistory
+# Print Response
